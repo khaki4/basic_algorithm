91e80b69fb37f3116a86b51fe9469bb06cc94b11
jiwonjung <4dakto@gmail.com>
2020-02-19 21:15:41 +0900

    update

--- data-structure/binaryTree.cpp
+++ data-structure/binaryTree.cpp
@@ -1,59 +1,61 @@
 #include <iostream>
-#include <algorithm>
-#include <vector>
 
 using namespace std;
+
 const int number = 15;
 
-// 하나의 노드 정보를 선언합니다
-typedef struct node* treePointer;
-typedef struct node {
-	int data;
-	treePointer leftChild;
-	treePointer rightChild;
-} node;
+typedef struct Node* pTree;
+typedef struct Node {
+    int data;
+    Node* leftChild;
+    Node* rightChild;
+} Node;
 
-// 전위 순회를 구현합니다.
-void preOrder(treePointer ptr) {
-	if (ptr) {
-		cout << ptr->data << ' ';
-		preOrder(ptr->leftChild);
-		preOrder(ptr->rightChild);
-	}
+void preOrder(Node* pTree) {
+    if (pTree) {
+        cout << pTree->data << ' ';
+        preOrder(pTree->leftChild);
+        preOrder(pTree->rightChild);
+    }
 }
 
-// 전위 순회를 구현합니다.
-void inOrder(treePointer ptr) {
-	if (ptr) {
-		inOrder(ptr->leftChild);
-		cout << ptr->data << ' ';
-		inOrder(ptr->rightChild);
-	}
+void inOrder(Node* pTree) {
+    if (pTree) {
+        inOrder(pTree->leftChild);
+        cout << pTree->data << ' ';
+        inOrder(pTree->rightChild);
+    }
 }
 
-// 후위 순회를 구현합니다.
-void postOrder(treePointer ptr) {
-	if (ptr) {
-		postOrder(ptr->leftChild);
-		postOrder(ptr->rightChild);
-		cout << ptr->data << ' ';
-	}
+void postOrder(Node* pTree) {
+    if (pTree) {
+        postOrder(pTree->leftChild);
+        postOrder(pTree->rightChild);
+        cout << pTree->data << ' ';
+    }
 }
 
 int main(void) {
-	node nodes[number + 1];
-	for (int i = 1; i <= number; i++) {
-		nodes[i].data = i;
-		nodes[i].leftChild = NULL;
-		nodes[i].rightChild= NULL;
-	}
-	for (int i = 1; i <= number; i++) {
-		if (i % 2 == 0) {
-			nodes[i / 2].leftChild = &nodes[i];
-		}
-		else {
-			nodes[i / 2].rightChild = &nodes[i];
-		}
-	}
-	postOrder(&nodes[1]);
+    Node nodes[number + 1];
+    for (int i = 1; i <= number; i++) {
+        nodes[i].data = i;
+        nodes[i].leftChild = nullptr;
+        nodes[i].rightChild = nullptr;
+    }
+    for (int i = 1; i <= number; i++) {
+        if (i % 2 == 0) {
+            nodes[i / 2].leftChild = &nodes[i];
+        } else {
+            nodes[i / 2].rightChild = &nodes[i];
+        }
+    }
+
+    preOrder(&nodes[1]);
+    cout << endl;
+    inOrder(&nodes[1]);
+    cout << endl;
+    postOrder(&nodes[1]);
+    cout << endl;
+
+    return 0;
 }
